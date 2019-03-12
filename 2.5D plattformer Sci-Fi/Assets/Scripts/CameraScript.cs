cd03dc7a92f96ec4ffc8f5ad02292f0ea7cbb35c
derPraktikant <48132349+derPraktikant@users.noreply.github.com>
2019-03-12 14:37:15 +0100

    major update1

---
+++ 2.5D plattformer Sci-Fi/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject Character;
+    public float yOffset, zOffset;
+
+    private Vector3 offsetV3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetV3 = new Vector3(0, yOffset, zOffset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Character.transform.position + offsetV3;
+    }
+}
--- 2.5D plattformer Sci-Fi/Assets/Scripts/PlayerScript.cs
+++ 2.5D plattformer Sci-Fi/Assets/Scripts/PlayerScript.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float halfedPlayerSize;
-    public float halfedPlayerWidth;
     public float jumpVelocity;
     public float betterFallFloat;
     public float maxSpeed;
     public float runningAcceleration;
     public float runningBreak;
+    public Animator anim;
 
-    private Vector3 offsetLeftV3, offsetRightV3;
     private Vector3 jumpVelocityV3;
+    private Vector3 allVelocityV3;
     private Vector3 betterFallForceV3;
     private Rigidbody playerRB;
     private float oldHeight;
@@ -24,8 +24,6 @@ public class PlayerScript : MonoBehaviour
         betterFallForceV3 = new Vector3(0, -betterFallFloat, 0);
         oldHeight = transform.position.y;
         playerRB = GetComponent<Rigidbody>();
-        offsetLeftV3 = new Vector3(-halfedPlayerWidth, 0, 0);
-        offsetRightV3 = new Vector3(halfedPlayerWidth, 0, 0);
     }
 
     // Update is called once per frame
@@ -33,17 +31,18 @@ public class PlayerScript : MonoBehaviour
     {
         Movement();
         betterFalling();
+        animationAndRotation();
         Debug.Log(isGrounded());
     }
 
     bool isGrounded()
     {
-        return (Physics.Raycast(transform.position + offsetLeftV3, Vector3.down, halfedPlayerSize) || Physics.Raycast(transform.position + offsetRightV3, Vector3.down, halfedPlayerSize));
+        return (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + halfedPlayerSize, transform.position.z), Vector3.down, halfedPlayerSize + 0.05f));
     }
 
     void Movement()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
             jumpVelocityV3 = playerRB.velocity;
             jumpVelocityV3.y += jumpVelocity;
@@ -51,9 +50,16 @@ public class PlayerScript : MonoBehaviour
         }
 
         //Debug.Log(Input.GetAxisRaw("Horizontal"));
-        if (Input.GetAxisRaw("Horizontal") != 0 && Mathf.Abs(playerRB.velocity.x) < maxSpeed)
+        if (Input.GetAxisRaw("Horizontal") != 0) // && Mathf.Abs(playerRB.velocity.x) < maxSpeed)
         {
             playerRB.AddForce(new Vector3(Input.GetAxis("Horizontal") * runningAcceleration, 0, 0));
+            allVelocityV3 = playerRB.velocity;
+            if(Mathf.Abs(allVelocityV3.x) > maxSpeed && Input.GetAxisRaw("Horizontal") != 0)
+            {
+                //Debug.Log("exceeded speed limit");
+                allVelocityV3.x = maxSpeed * Mathf.Sign(allVelocityV3.x);
+                playerRB.velocity = allVelocityV3;
+            }
         }
         else if (Input.GetAxisRaw("Horizontal") == 0 && Mathf.Abs(playerRB.velocity.x) > 1)
         {
@@ -68,5 +74,19 @@ public class PlayerScript : MonoBehaviour
             playerRB.AddForce(betterFallForceV3);
         }
         oldHeight = transform.position.y;
+    }
+
+    void animationAndRotation()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 90 * Mathf.Sign(playerRB.velocity.x), 0));
+        }
+        anim.SetFloat("MoveSpeed", Mathf.Abs(playerRB.velocity.x) / maxSpeed, 0.1f, Time.deltaTime);
+        anim.SetBool("Grounded", isGrounded());
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            anim.SetTrigger("Jump");
+        }
     }
 }
